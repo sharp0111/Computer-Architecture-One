331cf7a5271405ead9f40abb7d049f9c0e4a1fac
Dani Tacheny <dani.tacheny@gmail.com>
2018-04-09 18:14:53 -0500

    add ability to run programs from other files

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,5 +1,6 @@
 const RAM = require('./ram');
 const CPU = require('./cpu');
+const fs = require('fs');
 
 /**
  * Load an LS8 program into memory
@@ -7,22 +8,26 @@ const CPU = require('./cpu');
  * TODO: load this from a file on disk instead of having it hardcoded
  */
 function loadMemory() {
+  // Hardcoded program to print the number 8 on the console
 
-    // Hardcoded program to print the number 8 on the console
+  const fileName = process.argv[2];
 
-    const program = [ // print8.ls8
-        "10011001", // LDI R0,8  Store 8 into R0
-        "00000000",
-        "00001000",
-        "01000011", // PRN R0    Print the value in R0
-        "00000000",
-        "00000001"  // HLT       Halt and quit
-    ];
+  var fs = require('fs');
+  var program = fs
+    .readFileSync(fileName)
+    .toString()
+    .split('\n')
+    .reduce((array, line) => {
+      if (line[0] !== '#' && line !== '') {
+        return array.concat(line.slice(0,8));
+      }
+      return array;
+    },[])
 
-    // Load the program into the CPU's memory a byte at a time
-    for (let i = 0; i < program.length; i++) {
-        cpu.poke(i, parseInt(program[i], 2));
-    }
+  // Load the program into the CPU's memory a byte at a time
+  for (let i = 0; i < program.length; i++) {
+    cpu.poke(i, parseInt(program[i], 2));
+  }
 }
 
 /**
